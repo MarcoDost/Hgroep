fcb85373fbbe2d7b9d36d444ae7ddcb523b6caba
aaron willem meinema <aaronwillem@hotmail.com>
2017-10-11 10:48:54 +0200

    logo somewhat centered

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 <html lang="en">
  <head>
   <meta charset="utf-8">
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnJfZX5O0jN2sATDiZx397U0oZTsiluHA&callback=initMap" type="text/javascript"></script>
-  <link rel="stylesheet" type="text/css" href="project.css">
+	<link rel="stylesheet" type="text/css" href="project.css">
   <title>Aaron Meinema</title>
  </head>
  <body>
-
   <div id="container">
-    <ul id="header">
-      <li class="header"><a class="active" href="index.php">Home</a></li>
-      <li class="header"><a href="locaties.php">Locaties</a></li>
-      <li class="header"><a href="opleidingen.php">Opleidingen</a></li>
-	    <li class="header"><a href="opendagen.php">Open dagen</a></li>
-      <img id="logomiddle" src="img/zwolelogowitklein.png" alt="logo">
-			<li id="right" class="header"><a href="search.php">search</a></li>
-  	</ul>
+    <nav>
+      <img src="img/zwolelogowitklein.png" alt="logo">
+      <ul id="header">
+        <li class="header"><a class="active" href="index.php">Home</a></li>
+        <li class="header"><a href="locaties.php">Locaties</a></li>
+        <li class="header"><a href="opleidingen.php">Opleidingen</a></li>
+  	    <li class="header"><a href="opendagen.php">Open dagen</a></li>
+  			<li id="right" class="header"><a href="search.php">search</a></li>
+    	</ul>
+    </nav>
   	<div id="main">
 			<h1>main</h1>
 		</div>
   	<footer>
-      <div id="footer1">
+      <div class="footerparts">
         <h2>Locaties</h2>
         <ul class="footer">
           <li><a href="locaties.php">Locaties</a></li>
@@ -29,28 +29,28 @@
     	    <li><a href="opendagen.php">Open dagen</a></li>
         </ul>
       </div>
-      <div id="footer2">
+      <div class="footerparts">
         <h2>Locaties</h2>
         <ul class="footer">
           <li><a href="locaties.php">Locaties</a></li>
           <li><a href="opleidingen.php">Opleidingen</a></li>
     	    <li><a href="opendagen.php">Open dagen</a></li>
       </div>
-      <div id="footer3">
+      <div class="footerparts">
         <h2>Locaties</h2>
         <ul class="footer">
           <li><a href="locaties.php">Locaties</a></li>
           <li><a href="opleidingen.php">Opleidingen</a></li>
     	    <li><a href="opendagen.php">Open dagen</a></li>
       </div>
-      <div id="footer4">
+      <div class="footerparts">
         <h2>Locaties</h2>
         <ul class="footer">
           <li><a href="locaties.php">Locaties</a></li>
           <li><a href="opleidingen.php">Opleidingen</a></li>
     	    <li><a href="opendagen.php">Open dagen</a></li>
       </div>
-      <div id="below">
+      <div id="botfooter">
         <p>Made by Aaron</p>
       </div>
     </footer>
