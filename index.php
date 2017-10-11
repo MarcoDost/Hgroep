7c1488801d24c61347072371bc452525c4dc075c
aaron willem meinema <aaronwillem@hotmail.com>
2017-10-11 11:21:01 +0200

    footer ready

--- index.php
+++ index.php
@@ -24,31 +24,39 @@
       <div class="footerparts">
         <h2>Locaties</h2>
         <ul class="footer">
-          <li><a href="locaties.php">Locaties</a></li>
-          <li><a href="opleidingen.php">Opleidingen</a></li>
-    	    <li><a href="opendagen.php">Open dagen</a></li>
+          <li><a href="zwolle.php">Zwolle</a></li>
+          <li><a href="emmen.php">Emmen</a></li>
+    	    <li><a href="assen.php">Assen</a></li>
+    	    <li><a href="meppel.php">Meppel</a></li>
         </ul>
       </div>
       <div class="footerparts">
-        <h2>Locaties</h2>
+        <h2>Studeren bij Zwolle hogeschool</h2>
         <ul class="footer">
-          <li><a href="locaties.php">Locaties</a></li>
-          <li><a href="opleidingen.php">Opleidingen</a></li>
-    	    <li><a href="opendagen.php">Open dagen</a></li>
+          <li><a href="aanmelden.php">Aanmelden, hoe doe je dat?</a></li>
+          <li><a href="studeren.php">Studeren, wat kost dat?</a></li>
+    	    <li><a href="beperking.php">Studeren met een functiebeperking</a></li>
+    	    <li><a href="topsport.php">Studeren en topsport</a></li>
+    	    <li><a href="bijbaan.php">Werken en Studeren</a></li>
+    	    <li><a href="ervaringen.php">Ervaringen van studenten</a></li>
+        </ul>
       </div>
       <div class="footerparts">
-        <h2>Locaties</h2>
+        <h2>Over Zwolle hogeschool</h2>
         <ul class="footer">
-          <li><a href="locaties.php">Locaties</a></li>
-          <li><a href="opleidingen.php">Opleidingen</a></li>
-    	    <li><a href="opendagen.php">Open dagen</a></li>
+          <li><a href="organisatie.php">Organisatie</a></li>
+          <li><a href="vacatures.php">Vacatures en stages</a></li>
+          <li><a href="kwaliteit.php">Kwaliteit en accreditatie</a></li>
+        </ul>
       </div>
       <div class="footerparts">
-        <h2>Locaties</h2>
+        <h2>Info voor</h2>
         <ul class="footer">
-          <li><a href="locaties.php">Locaties</a></li>
-          <li><a href="opleidingen.php">Opleidingen</a></li>
-    	    <li><a href="opendagen.php">Open dagen</a></li>
+          <li><a href="alumni.php">Alumni</a></li>
+          <li><a href="ouders.php">Ouders</a></li>
+    	    <li><a href="decanen.php">Decanen</a></li>
+    	    <li><a href="zakendoen.php">Zakendoen met Stenden</a></li>
+        </ul>
       </div>
       <div id="botfooter">
         <p>Made by Aaron</p>
