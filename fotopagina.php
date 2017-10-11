7fd633348626592a0a545a213eb6f6d96145708e
LAPTOP-4DG0SJ0S\lyh <asdfhenk@gmail.com>
2017-10-11 10:43:17 +0200

    Beginnetje aan fotopagina en zorgen dat de branch werkt enzo

---
+++ fotopagina.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="project.css">
+		<title>Yon Wierda</title>
+	</head>
+	
+	<body>
+		<div id="container">
+			<ul id="header">
+				<li class="header"><a class="active" href="index.php">Home</a></li>
+				<li class="header"><a href="locaties.php">Locaties</a></li>
+				<li class="header"><a href="opleidingen.php">Opleidingen</a></li>
+				<li class="header"><a href="opendagen.php">Open dagen</a></li>
+				<img id="logomiddle" src="img/zwolelogowitklein.png" alt="logo">
+				<li id="right" class="header"><a href="search.php">search</a></li>
+			</ul>
+			
+			<div id="main">
+				hoi
+			<div>
+		</div>
+	</body>
+</html>
