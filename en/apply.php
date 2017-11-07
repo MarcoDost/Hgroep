46d53cc63d39c735d153336e9d2d8264ed7a88e8
sidoi <sidoiklumper@gmail.com>
2017-11-07 20:12:41 +0100

    fixed some shit

--- en/apply.php
+++ en/apply.php
@@ -21,21 +21,20 @@
 
 			<ul class="right hide-on-med-and-down">
 				<li><a href="index.html">Home</a></li>
-				<li><a href="locaties.html">Locaties</a></li>
-				<li><a href="opleidingen.html">Opleidingen</a></li>
-				<li><a href="inschrijven.php">Inschrijven</a></li>
-				<li><a href="schoolinfo.html">Over de school</a></li>
-				
-				<li><a href="fotos.html">Foto's</a></li>
+				<li><a href="locations.html">Locations</a></li>
+				<li><a href="courses.html">Courses</a></li>
+				<li><a href="apply.php">Apply</a></li>
+				<li><a href="information.html">College information</a></li>
+				<li><a href="pictures.html">Pictures</a></li>
 			</ul>
 
 			<ul id="nav-mobile" class="side-nav">
 				<li><a href="index.html">Home</a></li>
-				<li><a href="locaties.html">Locaties</a></li>
-				<li><a href="opleidingen.html">Opleidingen</a></li>
-				<li><a href="inschrijven.html">Inschrijven</a></li>
-				<li><a href="schoolinfo.html">Over de school</a></li>
-				<li><a href="fotos.html">Foto's</a></li>
+				<li><a href="locations.html">Locations</a></li>
+				<li><a href="courses.html">Courses</a></li>
+				<li><a href="apply.php">Apply</a></li>
+				<li><a href="information.html">College information</a></li>
+				<li><a href="pictures.html">Pictures</a></li>
 			</ul>
 
 			<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
@@ -43,8 +42,8 @@
 	</nav>
 
 	<br><br><div class="container">
-		<center><h1>Inschrijven</h1>
-			<h4>Hier kun je je inschrijven voor onze opleidingen, doe het snel nu het nog kan!</h4></center><br><br>
+		<center><h1>Application</h1>
+			<h4>On this page you can apply for our studies, do it now while you still can!</h4></center><br><br>
 			<form method="post">
 				<?php
 				$nameErr = $emailErr = $genderErr = $websiteErr = "";
@@ -122,14 +121,14 @@
 
 				<div class="card white">
 					<div class="card-content black-text">
-						<span class="card-title">Inschrijfformulier</span>
+						<span class="card-title">Application form</span>
 						<p>
 							<div class="row">
 								<form class="col s12" method = "POST" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 									<div class="row">
 										<div class="input-field col s6">
 											<input placeholder="Hans Jansen" id="name" type="text" class="validate" required>
-											<label for="name">* Volledige naam</label>
+											<label for="name">* Full name</label>
 											<span class = "error"><?php echo $nameErr;?></span>
 										</div>
 										<div class="input-field col s6">
@@ -143,7 +142,7 @@
 									<div class="row">
 										<div class="input-field col s12">
 											<input placeholder="0612345678" id="course" type="text">
-											<label for="course">Telefoon nummer</label>
+											<label for="course">Phone number</label>
 											<span class = "error"><?php echo $websiteErr;?></span>
 										</div>
 									</div>
@@ -151,32 +150,32 @@
 									<div class="row">
 										<div class="input-field col s12">
 											<textarea id="opmerkingen" name="text" class="materialize-textarea" data-length="256"></textarea>
-											<label for="opmerkingen">Eventuele opmerkingen</label>
+											<label for="opmerkingen">Optional notes</label>
 										</div>
 									</div>
 
-									* Geslacht:
+									* Gender:
 									<p>
 										<input name="gender" type="radio" id="test1" value="vrouw" checked />
-										<label for="test1">Vrouw</label>
+										<label for="test1">Female</label>
 									</p>
 									<p>
 										<input name="gender" type="radio" id="test2" value="man"/>
-										<label for="test2">Man</label>
+										<label for="test2">Male</label>
 									</p>
 									<span class = "error"><?php echo $genderErr;?></span>
-									<br><br>* Opleiding:
+									<br><br>* Course:
 									<p>
 										<input name="subject[]" type="radio" id="test3" value="Informatica" checked />
-										<label for="test3">Informatica</label>
+										<label for="test3">Informatics</label>
 									</p>
 									<p>
 										<input name="subject[]" type="radio" id="test4" value="Werktuigbouwkunde"/>
-										<label for="test4">Werktuigbouwkunde</label>
+										<label for="test4">Mechanical Engineering</label>
 									</p>
 									<br><br>
-									<span class = "error">* <?php echo "Hiermee geef je aan akkoord te gaan met onze Terms of agreement (verplicht)<br>";?></span>
-									<span class = "error">* Verplicht veld.</span>
+									<span class = "error">* <?php echo "By applying you accept out terms of service.<br>";?></span>
+									<span class = "error">* Required field.</span>
 								</form>
 							</p>
 						</div>
@@ -208,25 +207,26 @@
 					<div class="row">
 
 						<div class="col l4 s12">
-							<h5 class="white-text">Locaties</h5>
+							<h5 class="white-text">Locations</h5>
 							<ul>
-								<li><a class="white-text" href="locaties.html">Locaties bekijken.</a></li>
+								<li><a class="white-text" href="locations.html">View our locations.</a></li>
 							</ul>
 						</div>
 
 						<div class="col l4 s12">
-							<h5 class="white-text">Studeren bij Zwolle hogeschool</h5>
+							<h5 class="white-text">Studying at Zwolle hogeschool</h5>
 							<ul>
-								<li><a class="white-text" href="inschrijven.php">Inschrijven</a></li>
-								<li><a class="white-text" href="schoolinfo.html">Meer info</a></li>
-								<li><a class="white-text" href="fotos.html">Foto's</a></li>
+								<li><a class="white-text" href="apply.php">Apply</a></li>
+								<li><a class="white-text" href="information.html">More information</a></li>
+								<li><a class="white-text" href="pictures.html">Pictures</a></li>
 							</ul>
 						</div>
 
 						<div class="col l4 s12">
-							<h5 class="white-text">Over Zwolle hogeschool</h5>
+							<h5 class="white-text">About Zwolle hogeschool</h5>
 							<ul>
-								<li><a class="white-text" href="schoolinfo.html">Meer informatie</a></li>
+								<li><a class="white-text" href="information.html">More information</a></li>
+								<li><a class="white-text" href="../index.html">Dutch / Nederlands</a></li>
 							</ul>
 						</div>
 
@@ -247,10 +247,5 @@
 			<script type="text/javascript">$("button").click(function () {
 				$('html,body').animate({scrollTop: $(".second").offset().top}, 'slow');
 			});</script>
-			<script type="text/javascript">
-				$(document).ready(function() {
-					$('input#input_text, textarea#textarea1').characterCounter();
-				});
-			</script>
 		</body>
 		</html>
