1ab3ead6e5d0c2f4963979d7148231a1405b243a
sidoi <sidoiklumper@gmail.com>
2017-11-09 00:57:11 +0100

    rss aangepast

--- rss.php
+++ rss.php
@@ -1,31 +1,25 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <rss version="2.0">
 	<channel>
-		<title>Test rss feed</title>
+		<title>RSS feed</title>
 		<link>https://sidoi.nl/</link>
 		<description>test rss feed</description>
 		<language>en-us</language>
 		<copyright>Copyright (C) 2017 sidoi.nl</copyright>
 		<item>
-			<title>html</title>
-			<description>html opdrachten opdracht 7</description>
-			<link>https://sidoi.nl/school/htmlopdrachten/opdracht7</link>
-			<pubDate>Mon, 23 Feb 2009 09:27:16 +0000</pubDate>
+			<title>Locaties zwolle hogeschool</title>
+			<description>Alle locaties van de zwolle hogeschool</description>
+			<link>https://sidoi.nl/school/zwollehogeschool/locaties.html</link>
+			<pubDate>Wed, 29 Oct 2017 15:57:20 +0000</pubDate>
 		</item>
 		<item>
-			<title>PHP</title>
-			<description>php school opdrachten</description>
-			<link>https://sidoi.nl/school/php</link>
-			<pubDate>Wed, 14 Jan 2009 12:00:00 +0000</pubDate>
+			<title>Over onze school</title>
+			<description>Diverse informatie over onze school</description>
+			<link>https://sidoi.nl/school/zwollehogeschool/schoolinfo.html</link>
+			<pubDate>Wed, 25 Oct 2017 15:57:20 +0000</pubDate>
 		</item>
 		<item>
 			<title>Zwolle hogeschool</title>
-			<description>De zwolle hogeschool pagina</description>
-			<link>https://sidoi.nl/school/zwollehogeschool/</link>
-			<pubDate>Wed, 05 Jan 2009 15:57:20 +0000</pubDate>
-		</item>
-		<item>
-			<title>Zwolle hogeschool 2</title>
 			<description>De zwolle hogeschool pagina</description>
 			<link>https://sidoi.nl/school/zwollehogeschool/</link>
 			<pubDate>Wed, 05 Oct 2017 15:57:20 +0000</pubDate>
